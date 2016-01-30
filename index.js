4d775515d9ab7df71cb4a6d58bc840609182a0c4
Tom McLean <tom.a.mclean@gmail.com>
2016-01-30 17:12:42 +1100

    Switch to using mersenne twister

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-var MASK = 0xffffffff;
+var MersenneTwister = require('mersenne-twister');
 
 function Generator(seed) {
-	this.seed(seed);
+	this.generator = new MersenneTwister(seed);
 }
 
-// TODO: replace with mersenne twister probably?
-// MWC, from http://stackoverflow.com/a/19301306
 Generator.prototype.random = function() {
-	// Returns number between 0 (inclusive) and 1.0 (exclusive),
-	// just like Math.random().
-    this.m_z = (36969 * (this.m_z & 65535) + (this.m_z >> 16)) & MASK;
-    this.m_w = (18000 * (this.m_w & 65535) + (this.m_w >> 16)) & MASK;
-    var result = ((this.m_z << 16) + this.m_w) & MASK;
-    result /= 4294967296;
-    return result + 0.5;
+    return this.generator.random();
 };
 
 Generator.prototype.integer = function(max) {
